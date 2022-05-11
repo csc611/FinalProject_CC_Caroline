1665fcf419b47059a9b676f4aa0a0975a9f90946
csc611 <47250670+csc611@users.noreply.github.com>
2022-05-11 16:50:25 -0400

    push
    push for work

--- main/sketch.js
+++ main/sketch.js
@@ -1,96 +1,202 @@
 //variables ------------------------------------------------------
-var scene = 0;
-
+var scene = 0 // the different game screens
 var fr; //FPS
 
 
 var pet;
 var petname = '';
+
 var homebutton;
-var pacifico;
+var pacifico; //font family
 
-var coins = 0;
-
-var playbutton;
-
+var affection = 0;
 var patcount = 0;
-
 var petting = false;
 
+var playbutton;
 var bathtub;
 var food;
 var bed;
 var custom;
 
+var lampState;
+
+var onlamp;
+var offlamp;
+
+var mealTimer;
+
+// customizeeee
+var leaf;
+var daisy;
+var baby;
+var halo;
+var scarf;
+
+
+//switches for hats
+
+var leafon;
+var daisyon;
+var babyon;
+var haloon;
+var scarfon;
+
+var triangleRice;
+
+//classes ------------------------------------------------------
+
+
+class bHeart {  
+  //these are the affection hearts that pop up when petting blobby
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+  
+  display() {
+    fill(255,0,0);
+    noStroke();
+    beginShape();
+    bezier(this.x,this.y,this.x-30,this.y-20,this.x-20,this.y-70,this.x,this.y-35);
+    bezier(this.x,this.y,this.x+30,this.y-20,this.x+20,this.y-70,this.x,this.y-35);
+    endShape();
+  }
+  
+}
+
 //preload --------------------------------------------------------
 function preload() {
   
-  //font family pacifico
+  //pacifico = loadFont('Pacifico-Regular.ttf');
   pacifico = loadFont('assets/Pacifico-Regular.ttf');
   
 }
 
 //setup -----------------------------------------------------------
 function setup() {
-  createCanvas(1000, 700);
+  createCanvas(windowWidth, windowHeight);
   
-  coins = 0;
+  //this is your affection count w blobby
+  affection = 0;
   
-  fr = 1; //starting FPS
-  frameRate(fr); //refresh to starting FPS
-  
+  fr = 1; //initial fps
+  frameRate(fr);
+
+  //text general settings
   textAlign(CENTER, CENTER);
   textFont(pacifico);
   
-  
-  pet = createSprite(width/2, height/2);
-  //create an animation
-  pet.addAnimation('assets/box0001.png','assets/box0002.png','assets/box0003.png');
-  
-  pet.onMousePressed = function (){  // petting the blobby will give the blobby affection and every 10 good pets will award 5 coins
+  //this is the blobby sprite <3
+  pet = createSprite(windowWidth/2, windowHeight/2);
+	
+  //blobby animation
+	
+  pet.addAnimation('b_normal','assets/blue/normal1.png','assets/blue/normal2.png','assets/blue/normal3.png');
+  pet.addAnimation('b_loved','assets/blue/loved1.png','assets/blue/loved2.png','assets/blue/loved3.png');
+  pet.addAnimation('b_snacky','assets/blue/snacky1.png','assets/blue/snacky2.png','assets/blue/snacky3.png');
+  pet.addAnimation('sleeping','assets/blue/sleeping1.png','assets/blue/sleeping2.png','assets/blue/sleeping3.png');
+	
+	
+  /*
+  pet.addAnimation('b_normal','normal1.png','normal2.png','normal3.png');
+  pet.addAnimation('b_loved','loved1.png','loved2.png','loved3.png');
+  pet.addAnimation('b_snacky','snacky1.png','snacky2.png','snacky3.png');
+  pet.addAnimation('sleeping','sleeping1.png','sleeping2.png','sleeping3.png');
+  */
+
+
+  // petting the blobby will give affection
+  pet.onMousePressed = function (){
+    pet.changeAnimation('b_loved');
     patcount += 1;
+    petting = true;
+    blobbyHearts = new bHeart(pet.position.x + (random -100,100),pet.position.y+ (random(-60,60))); 
     console.log(patcount);
-    
-    if (patcount > 0 && patcount % 10 == 0) {
-      coins += 5;
-      console.log(coins);
+		
+	//every 5 good pets will award 1 affection point
+    if (patcount > 0 && patcount % 5 == 0) {
+      affection += 1;
+      console.log(affection);
     }
   }
   
-  //buttons for events
-  playbutton = createImg('/assets/play.png');
-
-  homebutton = createImg('assets/home_btn.png');
+  //create  image buttons for event
   
+/*
+  playbutton = createImg('play.png');
+  homebutton = createImg('home_btn.png');
+  bathtub = createImg('Bathtub.png');
+  food = createImg('food.png');
+  bed = createImg('bed.png');
+  custom = createImg('art.png');
+  onlamp = createImg('onlamp.png');
+  offlamp = createImg('offlamp.png');
+  leaf = createImg('leaf.png');
+  daisy = createImg('daisy.png');
+  baby = createImg('babyblob.png');
+  halo = createIImg('halo.png');
+  scarf = createImg('scarf.png');
+  triangleRice = createImg('trianglerice.png');
+  */
+  
+  playbutton = createImg('assets/play.png');
+  homebutton = createImg('assets/home_btn.png');
   bathtub = createImg('assets/Bathtub.png');
   food = createImg('assets/food.png');
   bed = createImg('assets/bed.png');
   custom = createImg('assets/art.png');
+  onlamp = createImg('assets/onlamp.png');
+  offlamp = createImg('assets/offlamp.png');
+  triangleRice = createImg('assets/trianglerice.png');
+  leaf = createImg('assets/leaf.png');
+  daisy = createImg('assets/daisy.png');
+  baby = createImg('assets/babyblob.png');
+  halo = createImg('assets/halo.png');
+  scarf = createImg('assets/scarf.png');
   
+  lampState = true;
 }
 
 
 
 //draw ------------------------------------------------------------
 function draw() {
-      
+  resizeCanvas(windowWidth, windowHeight);
   background(189,255,234);
+  
+  //make sure the buttons are hidden at first (I want to show them only in specific scenes)
   playbutton.hide();
   bathtub.hide();
   food.hide();
   bed.hide();
   custom.hide();
   homebutton.hide();
+  
+  onlamp.hide();
+  offlamp.hide();
+  
+  leaf.hide();
+  daisy.hide();
+  baby.hide();
+  halo.hide();
+  scarf.hide();
+  
+  triangleRice.hide();
+  
 
-  if (scene ==0) {           //title screen 
+  if (scene ==0) {       //title screen
     playbutton.show();
 
+    //making variables to randomize the color of the game title
     var r = 7;
     var g = 144;
     var b = 162;
 
     textSize(100);
     
+    //continuously changing the color of the BLOBBY title
+    //framerate is set to 1, so it's a very slow animation
     for(var i = 0; i<100; i++) {
       r += random(-5,5);
       g += random(-5,5);
@@ -98,101 +204,250 @@ function draw() {
       fill(r,g,b);
     }
     
-    text('blobby',500,250);
+    // game title
+    text('blobby',windowWidth/2,250);
     
+    
+    // readjusting the text properties so only the game title is changing colors
     textSize(30);
-    fill (0,0,0);
-    text('your',500,180);
+    fill (0,0,0); 
+    text('your',windowWidth/2,180);
     
     
-    //play button to start game
-    playbutton.position(430,350);    
+    //press the play button to start game
+    playbutton.position(windowWidth/2-100,windowHeight/2); 
     playbutton.mousePressed(switchToScene1);
 
     
   }else if (scene == 1){       //user input pet name    
-    line(200,350,800,350);
+    //this is the line where you write the new name
+	line(100,windowHeight/2 +50,windowWidth-100,windowHeight/2 +50);
     
+    //instructions
     textSize(30);
-    text('give your blobby a name',400,180);
-    textSize(50);
-    text(petname, 500,300);
+    text('give your blobby a name',windowWidth/2-100,180);
     
-  }else if (scene == 2) {           // main game screen
-    bathtub.show();
+	//this is your blobby's new name!
+    textSize(50);
+    text(petname, windowWidth/2,windowHeight/2);
+    
+  }else if (scene == 2) {       // main game screen
+    
+	//show the hidden buttons
+	bathtub.show();
     food.show();
     bed.show();
     custom.show();
     
+    
+	//adjusting framerate
     fr = 30;
     frameRate(fr);
+		
+	//drawing your blobby and its new name!
+	pet.position.x = windowWidth/2;
+	pet.position.y = windowHeight/2; // making sure pet stays in center of window
+    pet.scale =1;
+    pet.changeAnimation('b_normal');
     drawSprite(pet);
-    textSize(15);
+    
+    textSize(20);
     text(petname,pet.position.x,pet.position.y-80);
     
-    //draw bathtub
+    //draw bathtub button
     bathtub.position(50,50);
     bathtub.mousePressed(switchBathEvent);
     
-    //draw food
-    food.position(50,500);
+    //draw food button
+    food.position(50,windowHeight-200);
     food.mousePressed(switchMealEvent);
 
     
-    //draw bed
-    bed.position(750,50);
+    //draw bed button
+    bed.position(windowWidth-200,50);
     bed.mousePressed(switchSleepEvent);
 
     
-    //draw customization
-    custom.position(750,500);
+    //draw customization button
+    custom.position(windowWidth-200,windowHeight-200);
     custom.mousePressed(switchCustomizations);
     
-    text("coins = " + str(coins), width - 50, 50);
+    if (windowWidth > 600) {
+        text("affection = " + str(affection), windowWidth/2, 50);
+    } else {
+        text("affection = " + str(affection), 50, windowHeight/2);
+    }
+
+    if (petting) {
+      blobbyHearts.display();
+      textSize(15);
+      petting = false;
+    }
+  
+
+    if (patcount > 0 && patcount % 5 == 0) {
+      text("+1", pet.position.x, pet.position.y-120);
+      pet.changeAnimation('b_loved');
+      drawSprite(pet);
+    }
     
   } else if (scene == 3) {             //BATHTUB EVENT
     homebutton.show();
     homebutton.position(50,50);
     homebutton.mousePressed(switchHome);
+    pet.scale =1;
     drawSprite(pet);
+    fr = 30;
+    frameRate(fr);
+		
+		
   } else if (scene == 4) {             //MEAL EVENT
     homebutton.show();
     homebutton.position(50,50);
     homebutton.mousePressed(switchHome);
+    
+    fr = 30;
+    frameRate(fr);
+    
+    triangleRice.position(100,windowHeight/2);
+    triangleRice.show();
+    triangleRice.mousePressed(foodSpinnies);
+    
+    fill(90,90,90);
+    rect (0,windowHeight-80,windowWidth,80);
+    
+    pet.position.x = windowWidth/2;
+    pet.position.y = windowHeight - 100;
+    pet.scale = 0.5;
     drawSprite(pet);
+    
+
+    //movement with mouse - https://molleindustria.github.io/p5.play/examples/index.html?fileName=sprite3.js
+    
+    if (mouseX < pet.position.x - 20) {
+      pet.mirrorX(1);
+    } else if (mouseX > pet.position.x + 20) {
+      pet.mirrorX(-1);
+    }
+    
+		
   } else if (scene == 5) {             //SLEEP EVENT
+    
     homebutton.show();
     homebutton.position(50,50);
     homebutton.mousePressed(switchHome);
+    pet.scale =1;
     drawSprite(pet);
+    
+    if (lampState) {
+      fr = 30;
+      frameRate(fr);
+      onlamp.show();
+      onlamp.position(100,windowHeight/2);
+      onlamp.mousePressed(lightSwitch);
+      pet.changeAnimation('b_normal');
+      drawSprite(pet);
+    } else {
+      offlamp.show();
+      offlamp.position(100,windowHeight/2);
+      offlamp.mousePressed(lightSwitch);
+      pet.changeAnimation('sleeping');
+      drawSprite(pet);
+      
+      fr = 3;
+      frameRate(fr);
+    
+      textSize(15);
+      text("zzz",pet.position.x + 100,pet.position.y);
+      fill('rgba(0, 0, 0, 0.68)');
+      rect(0,0,windowWidth,windowHeight);
+    }
+    
+    
+    
   } else if (scene == 6) {             //CUSTOMIZATIONS
+    
     homebutton.show();
     homebutton.position(50,50);
     homebutton.mousePressed(switchHome);
     drawSprite(pet);
+    
+    leaf.position(windowWidth-250,50);
+    leaf.show();
+    
+    daisy.position(windowWidth-250,150);
+    daisy.show();
+    daisy.mousePressed(wearDaisy);
+    
+    if (daisyon) {
+      daisy.position(pet.position.x-100,pet.position.y-100);
+      text("Yes! That one!", pet.position.x-200, pet.position.y-100);
+    }
+
+    baby.position(windowWidth-250,250);
+    baby.show();
+
+    halo.position(windowWidth-250,300);
+    halo.show()
+    
+    scarf.position(windowWidth-250,350);
+    scarf.show()
+    
   }
 
 }
 
 
 function keyPressed() {
-  if (scene == 1) {
+  
+  if (scene == 1) {    //only happens when you are giving your blobby a name!
+	textSize(20);
+     
+    // you picked a good name! <3
+    
     if (petname.length > 3 && keyCode === ENTER) {
       scene = 2;
-    } else if (petname.length < 3 && keyCode === ENTER){
-      text("that name is too short!",430,550);      
-    } else if (petname.length > 20) {
-      text("that name is too long!",430,550);
-      return false;
-    } else if (keyCode === BACKSPACE){
-      petname = petname.substring(0, petname.length - 1);      
-      // erasing the text doesn't work, maybe because the original petname is already printed
-    } else if (keyCode > 64 && keyCode < 91 || keyCode > 96 && keyCode < 123){
+    } 
+    
+    // name too short! (must be at least 3 characters)
+    
+    else if (petname.length < 3 && keyCode === ENTER){  
+	  text("that name is too short!",windowWidth/2 - 20,windowHeight/2 + 150);   
+    } 
+    
+    // name too long! (cannot be longer than 20 characters)
+    
+    else if (petname.length > 20) {
+      text("that name is too long!",windowWidth/2 - 10,windowHeight/2 + 150);
+	  if (keyCode > 64 && keyCode < 91 || keyCode > 96 && keyCode < 123){
+      	return false;
+        //aka stop the typing inputs if you type a lowercase or uppercase letter
+	  } else if (keyCode === BACKSPACE){
+		petname = petname.substring(0, petname.length - 1);
+        //if name too long, user can ONLY erase letter (cannot add more letters)
+	  }
+    } 
+    
+    // user made an error while thinking of a name! please erase a letter (or more)
+    
+    else if (keyCode === BACKSPACE){
+      // petname is now the what the user typed but MINUS one letter
+      // aka return part of the petname string only from the beginning to the second to last letter
+      petname = petname.substring(0, petname.length - 1); 
+    } 
+    
+    // only type in uppercase/lowercase letters allowed! 
+    // hmph, sorry-- no blobby X Æ A-12's.
+    
+    else if (keyCode > 64 && keyCode < 91 || keyCode > 96 && keyCode < 123){
       petname += key;
     } else {
-      text("that is not a letter!",430,500);
+      text("that is not a letter!",windowWidth/2,windowHeight/2 + 150);
     }
-  } /*else if (scene == 2) {
+  } 
+  
+    //to move the blobby around (probably will not include in the home screen)
+  /*else if (scene == 2) {
     if (key == "a") {
       pet.position.x += -20;
     } else if (key == "d") {
@@ -205,6 +460,28 @@ function keyPressed() {
   }
   */
 }
+
+
+
+function foodSpinnies() {
+  
+}
+
+
+
+function lightSwitch() {  //switch the lamp state on or off
+  if (lampState) {
+    lampState = false;
+  } else {
+    lampState = true;
+  }
+}
+
+
+function wearDaisy(){
+  daisyon = true;
+}
+
 
 //switches
 
